b946165fb0d385cd0503a30370c512dc68c0edcb
U-NICKSCOMPUTER\Nicholas <nmartucc@u.rochester.edu>
2016-08-03 15:31:45 -0400

    Added off road collision detection and speed setting

--- Assets/KartController.cs
+++ Assets/KartController.cs
@@ -1,21 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO.Ports;
 
 public class KartController : MonoBehaviour {
 
 	public bool showItem = false;
+	SerialPort sPort = new SerialPort("COM8", 9600);
+	public static double speed = 1;
 	
 	void Start ()
 	{
-
+		sPort.Open ();
 	}
-	
+		
 	void OnTriggerEnter(Collider other) 
 	{
-		if (other.gameObject.CompareTag ("Item Box"))
-		{
+		if (other.gameObject.CompareTag ("Off Road")) {
+			speed = 0.5;
+			if (sPort.IsOpen) {
+				sPort.WriteLine (speed + ",");
+			}
+		}
+		else if (other.gameObject.CompareTag ("Item Box")) {
 			showItem = true;
-			StartCoroutine(respawn(other,7));
+			StartCoroutine (respawn (other, 7));
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.CompareTag("Off Road")){
+			speed = 1;
+			if (sPort.IsOpen) {
+				sPort.WriteLine (speed + ",");
+			}
 		}
 	}
 
@@ -24,6 +42,10 @@ public class KartController : MonoBehaviour {
 		obj.gameObject.SetActive (false);
 		yield return new WaitForSeconds(respawnTime);
 		obj.gameObject.SetActive (true);
+	}
+
+	void OnApplicationQuit(){
+		sPort.Close();
 	}
 
 
