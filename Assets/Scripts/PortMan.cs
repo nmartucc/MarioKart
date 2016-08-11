957a953bfca9292121effa10b6765dfd594feb95
nmartucc <nmartucc@u.rochester.edu>
2016-08-11 14:55:41 -0400

    Added PortMan

---
+++ Assets/Scripts/PortMan.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class PortMan : MonoBehaviour {
+
+    public static SerialPort sPort = new SerialPort("COM8", 115200);
+
+    void Start()
+    {
+        sPort.Open();
+    }
+
+    public static void Write(int data)
+    {
+        if (sPort.IsOpen)
+        {
+            sPort.WriteLine(data + ",");
+        }
+    }
+
+    public static double[] Read()
+    {
+        double[] dist = null;
+        if (sPort.IsOpen)
+        {
+            try
+            {
+                sPort.DiscardInBuffer();
+                string rawData = sPort.ReadLine();
+                if (rawData.Length > 2)
+                {
+                    char delimiter = ',';
+                    string[] distancestrings = rawData.Split(delimiter);
+                    dist = new double[distancestrings.Length];
+                    for (int i = 0; i < distancestrings.Length - 1; i++)
+                    {   //converts each string in the array to a double
+                        dist[i] = double.Parse(distancestrings[i]);
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+            }
+        }
+        return dist;
+    }
+
+    void OnApplicationQuit()
+    {
+        sPort.Close();
+    }
+}
